42506b79e80918fc466ee7339f6eb47a63862c80
kheeva <kheeva@ya.ru>
2017-11-14 17:52:34 +0300

    simple json pprinter with readme and sample json file

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,37 @@
+import sys
 import json
 
 
-def load_data(filepath):
-    pass
+def load_data(file_path):
+    try:
+        with open(file_path, 'r', encoding='utf8') as json_file:
+            json_data = json.load(json_file)
+    except FileNotFoundError as error:
+        print(error)
+    except json.JSONDecodeError as error:
+        print(error)
+    else:
+        return json_data
+    return {}
 
 
 def pretty_print_json(data):
-    pass
+    print(json.dumps(data, indent=4, ensure_ascii=False))
 
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python pprint_json.py path_to_json_file.")
+        exit(1)
+
+    file_path = sys.argv[1]
+    json_data = load_data(file_path)
+
+    if not json_data:
+        print('No data!')
+        exit(1)
+
+    pretty_print_json(json_data)
 
 if __name__ == '__main__':
-    pass
+    main()
