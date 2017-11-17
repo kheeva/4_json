734eabfb5acd68c5d113a88ad1ad149b913c65ff
kheeva <kheeva@ya.ru>
2017-11-17 13:39:28 +0300

    load_data() was renamed to load_json_data(). var data was renamed to loaded_data

--- pprint_json.py
+++ pprint_json.py
@@ -2,36 +2,29 @@ import sys
 import json
 
 
-def load_data(file_path):
+def load_json_data(file_path):
+    with open(file_path, 'r', encoding='utf8') as json_file:
+        loaded_data = json.load(json_file)
+    return loaded_data
+
+
+def pretty_print_json(loaded_data):
+    print(json.dumps(loaded_data, indent=4, ensure_ascii=False))
+
+
+def main():
+    if len(sys.argv) != 2:
+        exit("Usage: python pprint_json.py path_to_json_file.")
+
     try:
-        with open(file_path, 'r', encoding='utf8') as json_file:
-            json_data = json.load(json_file)
+        loaded_data = load_json_data(sys.argv[1])
     except FileNotFoundError as error:
         print(error)
     except json.JSONDecodeError as error:
         print(error)
     else:
-        return json_data
-    return {}
+        pretty_print_json(loaded_data)
 
-
-def pretty_print_json(data):
-    print(json.dumps(data, indent=4, ensure_ascii=False))
-
-
-def main():
-    if len(sys.argv) != 2:
-        print("Usage: python pprint_json.py path_to_json_file.")
-        exit(1)
-
-    file_path = sys.argv[1]
-    json_data = load_data(file_path)
-
-    if not json_data:
-        print('No data!')
-        exit(1)
-
-    pretty_print_json(json_data)
 
 if __name__ == '__main__':
     main()
